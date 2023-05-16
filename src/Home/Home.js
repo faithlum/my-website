ded3c465b51c128e60b12839c4dde6f868b024ad
faith335 <faith335@users.noreply.github.com>
2023-05-16 18:48:48 -0400

    wip

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,9 +2,23 @@ import React from "react";
 
 function Home() {
     return (
-        <div>
-            <h1>Hi</h1>
-        </div>
+        <>
+            <div className="myName">
+            {/* <h2 className="name">faith lum</h2> */}
+            </div>
+            <div className="bodyHome">
+                <section className="home">
+                    <div className="home-content">
+                        <h1>welcome to my memory palace</h1>
+                        <p>an unofficial portfolio  .   .   . by Faith Lum</p>
+                        {/* <a href="#" className="btn">Documentation</a> */}
+                     </div>
+                    <div className="home-img">
+                        <img src="/website_profile1.jpg" alt=""/>
+                    </div>
+                </section>
+            </div>
+        </>
     )
 }
 
