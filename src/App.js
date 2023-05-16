87888c7ac7f3cd4bb5236930d393f6823b72d417
faith335 <faith335@users.noreply.github.com>
2023-05-16 18:40:29 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './Home/Home';
 
 function App() {
